b99ad2a4cbf0699349c40fbe5e64e0a12bc83506
Mirko Rüther <mirko.ruether@web.de>
2014-11-27 21:52:07 +0100

    HtmlReader angefangen, FileReader überarbeitet: arbeitet intern mit java.io.File anstelle von Strings

--- src/com/facharbeit/io/HtmlReader.java
+++ src/com/facharbeit/io/HtmlReader.java
@@ -5,8 +5,10 @@
  */
 package com.facharbeit.io;
 
-import com.facharbeit.tools.SchoolClass;
-import com.facharbeit.io.*;
+import com.facharbeit.io.FileReader;
+import com.facharbeit.tools.*;
+import java.io.*;
+import java.util.*;
 
 /**
  *
@@ -17,25 +19,74 @@ public class HtmlReader
 
     public static SchoolClass[] readInToday()
     {
-        return readInPath(Settings.load("sourcePath") + "/heute");
+        if(Settings.load("useSQL").equals("true"))
+            return readInSql();
+        else
+            return readInHtml(Settings.load("sourcePath") + "/heute");
     }
 
     public static SchoolClass[] readInTomorrow()
     {
-        return readInPath(Settings.load("sourcePath") + "/morgen");
+        if(Settings.load("useSQL").equals("true"))
+            return readInSql();
+        else
+            return readInHtml(Settings.load("sourcePath") + "/morgen");
     }
 
-    private static SchoolClass[] readInPath(String path)
+    private static SchoolClass[] readInHtml(String path)
+    {
+
+        String[] extraGrades =
+        {
+            "EF", "Q1", "Q2", "___"
+        };
+
+        ArrayList<File> files = new ArrayList<>();
+        File f;
+        for(int grade = 5; grade <= 9; grade++)
+            for(int c = 97; c <= 122; c++)
+            {
+                f = new File(path + "Druck_Klasse_0" + grade + "" + (char)c + ".htm");
+                if(f.exists())
+                    files.add(f);
+            }
+
+        for(String s : extraGrades)
+        {
+            f = new File(path + "Druck_Klasse_" + s + ".htm");
+            if(f.exists())
+                files.add(f);
+        }
+
+        SchoolClass[] outcome = new SchoolClass[files.size()];
+
+        for(int i = 0; i < outcome.length; i++)
+        {
+            outcome[i] = new SchoolClass(files.get(i).getName().substring(13, files.get(i).getName().lastIndexOf('.') - 1));
+
+            if(outcome[i].isEmpty())
+                outcome[i].setEntrys(new ArrayList<Entry>());
+
+            String fileAsString = new FileReader(files.get(i)).toString();
+            fileAsString = fileAsString.substring(fileAsString.indexOf("<TABLE border=\"3\" rules=\"all\" bgcolor=\"#E7E7E7\" cellpadding=\"1\" cellspacing=\"1\">"));
+            fileAsString = fileAsString.substring(0, fileAsString.indexOf("<TABLE cellspacing=\"1\" cellpadding=\"1\">") - 1);
+            boolean endOfFile = false;
+            while(!endOfFile)
+            {
+                fileAsString = fileAsString.substring(fileAsString.indexOf("<TR>") + 4);
+
+                //todo
+                if(!fileAsString.contains("<TR>"))
+                    endOfFile = true;
+            }
+        }
+
+        return outcome;
+    }
+
+    private static SchoolClass[] readInSql()
     {
         SchoolClass[] outcome = new SchoolClass[1];
-
-        if(Settings.load("useSQL").equals("true"))
-        {
-            //krasse SQL Eskalation
-        } else
-        {
-            //nicht so krasse String umwandel Eskalation
-        }
 
         return outcome;
     }
