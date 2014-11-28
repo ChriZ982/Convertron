ebd2c9c2c8b6638fd2274b1a6824798fdaf57c41
Mirko Rüther <mirko.ruether@web.de>
2014-11-28 21:01:02 +0100

    Sonderzeichen-Fix

--- src/com/facharbeit/io/FileReader.java
+++ src/com/facharbeit/io/FileReader.java
@@ -15,6 +15,12 @@ public class FileReader
 //    private String path;
 
     private File file;
+    private String charset = "UTF-8";
+
+    public void setCharset(String charset)
+    {
+        this.charset = charset;
+    }
 
     /**
      * Erstellt einen neuen Reader.
@@ -43,7 +49,7 @@ public class FileReader
      */
     public String read(int line)
     {
-        try(BufferedReader reader = new BufferedReader(new java.io.FileReader(file)))
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), charset)))
         {
             for(int i = 0; i < line; i++) //Läuft Zeilen durch bis die gewünschte Zeile erreicht ist.
                 reader.readLine();
@@ -66,7 +72,7 @@ public class FileReader
         int length = getLines();
         String[] data = new String[length];
 
-        try(BufferedReader reader = new BufferedReader(new java.io.FileReader(file)))
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), charset)))
         {
             for(int i = 0; i < length; i++)
                 data[i] = reader.readLine();
@@ -134,7 +140,7 @@ public class FileReader
      */
     public int getLines()
     {
-        try(BufferedReader reader = new BufferedReader(new java.io.FileReader(file)))
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), charset)))
         {
             int lines = 0;
             while(reader.readLine() != null)
