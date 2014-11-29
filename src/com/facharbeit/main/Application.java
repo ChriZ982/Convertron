372697b645fed1a01b761305bfc12a1d12936c44
ChriZ982 <christopher.zinda2@googlemail.com>
2014-11-29 20:04:49 +0100

    Tray added

--- src/com/facharbeit/main/Application.java
+++ src/com/facharbeit/main/Application.java
@@ -1,9 +1,14 @@
 package com.facharbeit.main;
 
 import com.facharbeit.io.*;
+import com.facharbeit.main.Frame;
 import com.facharbeit.tools.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
 import java.lang.reflect.*;
 import java.util.*;
+import javax.imageio.*;
 import javax.swing.*;
 
 /**
@@ -23,16 +28,12 @@ public class Application
     private boolean running;
 
     private static ArrayList<QueueElement> queue;
-    private static boolean doingStuff;
 
     /**
      * Erstellt die Anwendung.
      */
     public Application()
     {
-        Time.forHtmlReading(true);
-        Time.forHtmlReading(false);
-        Time.forHtmlReading(true);
         queue = new ArrayList<QueueElement>();
 
         try
@@ -49,6 +50,8 @@ public class Application
         Settings.init();
 
         frame.loadSettings();
+
+        initTray();
 
         running = true;
     }
@@ -112,26 +115,132 @@ public class Application
         }
     }
 
-    /**
-     * Generiert einen zufälligen "Satz".
-     *
-     * @deprecated
-     * @return "Zufalls-Satz"
-     */
-    private String randomString()
+    private void initTray()
     {
-        Random rand = new Random();
-        int length = rand.nextInt(60) + 20;
-        String s = "";
-
-        for(int i = 0; i < length; i++)
+        BufferedImage icon;
+        try
         {
-            if(rand.nextInt(5) == 0)
-                s += " ";
-
-            s += Character.toLowerCase((char)(rand.nextInt(26) + 65));
+            icon = ImageIO.read(getClass().getResource("/com/facharbeit/ressources/trayLogo.png"));
+        } catch(IOException ex)
+        {
+            Logger.log("Tray konnte nicht initialisiert werden", 2);
+            return;
         }
 
-        return s;
+        PopupMenu popup = new PopupMenu();
+        TrayIcon trayIcon = new TrayIcon(icon);
+        SystemTray tray = SystemTray.getSystemTray();
+
+        MenuItem genAll = new MenuItem("Alles generieren");
+        MenuItem genToday = new MenuItem("Heute generieren");
+        MenuItem genTomorrow = new MenuItem("Morgen generieren");
+        MenuItem genMotd = new MenuItem("Laufschrift generieren");
+        MenuItem backup = new MenuItem("Backup erstellen");
+        MenuItem removeSources = new MenuItem("Quelldateien löschen");
+        MenuItem show = new MenuItem("Maximieren");
+        MenuItem hide = new MenuItem("Minimieren");
+        MenuItem exitItem = new MenuItem("Beenden");
+
+        setMenuItems(genAll, genToday, genTomorrow, genMotd, backup, removeSources, show, hide, exitItem);
+
+        popup.add(genAll);
+        popup.add(genToday);
+        popup.add(genTomorrow);
+        popup.add(genMotd);
+        popup.addSeparator();
+        popup.add(backup);
+        popup.add(removeSources);
+        popup.addSeparator();
+        popup.add(show);
+        popup.add(hide);
+        popup.addSeparator();
+        popup.add(exitItem);
+
+        trayIcon.setPopupMenu(popup);
+        trayIcon.setToolTip("Vertretungsplan-Generator");
+
+        try
+        {
+            tray.add(trayIcon);
+        } catch(AWTException ex)
+        {
+        }
+    }
+
+    private void setMenuItems(MenuItem genAll, MenuItem genToday, MenuItem genTomorrow, MenuItem genMotd, MenuItem backup,
+                              MenuItem removeSources, MenuItem show, MenuItem hide, MenuItem exitItem)
+    {
+        genAll.addActionListener(new java.awt.event.ActionListener()
+        {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                Application.addToQueue("genAllBtnActionPerformed");
+            }
+        });
+        genToday.addActionListener(new java.awt.event.ActionListener()
+        {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                Application.addToQueue("genTodayBtnActionPerformed");
+            }
+        });
+        genTomorrow.addActionListener(new java.awt.event.ActionListener()
+        {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                Application.addToQueue("genTomorrowBtnActionPerformed");
+            }
+        });
+        genMotd.addActionListener(new java.awt.event.ActionListener()
+        {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                Application.addToQueue("genMotdBtnActionPerformed", new JTextField());
+            }
+        });
+        backup.addActionListener(new java.awt.event.ActionListener()
+        {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                Application.addToQueue("createBackupBtnActionPerformed");
+            }
+        });
+        removeSources.addActionListener(new java.awt.event.ActionListener()
+        {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                Application.addToQueue("deleteSourceBtnActionPerformed");
+            }
+        });
+        show.addActionListener(new java.awt.event.ActionListener()
+        {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                frame.setVisible(true);
+            }
+        });
+        hide.addActionListener(new java.awt.event.ActionListener()
+        {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                frame.setVisible(false);
+            }
+        });
+        exitItem.addActionListener(new java.awt.event.ActionListener()
+        {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                System.exit(0);
+            }
+        });
     }
 }
